7a51ddb3ac1b102c1a0b24db3335639a70376ba4
Washimaru <102931631+Washimaru@users.noreply.github.com>
2024-10-22 15:06:13 -0400

    Revert "Update StartMenuController.cs"
    This reverts commit 1ca17f8d0d945c0476de158f17d63b9a6886a81f.

--- Assets/Scripts/UI/StartMenuController.cs
+++ Assets/Scripts/UI/StartMenuController.cs
@@ -3,25 +3,38 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+
 public class StartMenuController : MonoBehaviour
 {
     //load the workout scene
     public void Start()
     {
+
+
         //load the main game scene
         SceneManager.LoadScene("TimedWorkOut");
+
+
     }
 
     //select the initial character
     public void InitialCharacter()
     {
+
+
         //load the initial character selection scene
         SceneManager.LoadScene("InitialCharacter");
+
+
     }
+
+
 
     //Exit game
     public void ExitGame()
     {
+        
         Application.Quit();
     }
+
 }
