ca94bb76de998e455f8ac75cba26fa0cb1ee489f
elizawq003 <elizawq12@gmail.com>
2024-11-14 23:48:09 -0500

    idea for shop scene

--- Assets/Scripts/UI/ShopSceneManager.cs
+++ Assets/Scripts/UI/ShopSceneManager.cs
@@ -4,6 +4,16 @@ using UnityEngine;
 
 public class ShopSceneManager : MonoBehaviour
 {
+    //have price labeled under each character
+
+    /*
+     * click:
+     * -1. enough money ask if purchase
+     *  - a. purchase -> store in unlocked characters scene and data
+     *  - b. else -> can exit the interactive panel
+     * - 2. not enough money -> different panel (can't purchase
+     */
+
     // Start is called before the first frame update
     void Start()
     {
